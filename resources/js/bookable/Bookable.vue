5e4d8c023007291b4a255087a81c9cc6627ec9bf
mocatfrio <hafarafirdausi@gmail.com>
2020-07-05 22:10:14 +0700

    add vuex

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -9,10 +9,10 @@
             <article> {{ bookable.description }} </article>
           </div>
         </div>
-        <review-list :bookable-id="this.$route.params.id.toString()"></review-list>
+        <review-list :bookable-id="this.$route.params.id"></review-list>
       </div>
       <div class="col-md-4 pb-4">
-        <availability :bookable-id="this.$route.params.id.toString()"></availability>
+        <availability :bookable-id="this.$route.params.id"></availability>
       </div>
     </div>
     <div v-else>
